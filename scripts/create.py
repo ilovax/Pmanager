fd7badfddc4e14d601f3c81c4e43918733a5f0f6
ilovax <a.tasfaout@esi-sba.dz>
2020-12-23 21:08:09 +0100

    Added create_pass password random generator

--- scripts/create.py
+++ scripts/create.py
@@ -1,11 +1,36 @@
 import getopt, sys
 import logging, coloredlogs
+import string, random
+from hashlib import sha1
 
 def log_config(loggin,coloredlogs):
 	logger = logging.getLogger(__name__)
 	coloredlogs.install(level='DEBUG', logger=logger)
 	coloredlogs.install(fmt='%(asctime)s | %(programname)s | %(levelname)s : %(message)s')
 
+# somme of characters code of a single word 
+def char_code_somme(word):
+	somme = 0
+	for i in word:
+		somme += ord(i)
+	return somme
+
+# if arguments are empty use  the string empty
+def create_pass(account="empty",secret="empty"):
+	# seed the radnom generator
+	randint = random.randint(0,99999999999)
+	random.seed(char_code_somme(secret)+randint)
+
+	password_length = 15 + len(secret)
+	random_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&()*+-<=>?@[]^_{|}~'
+	
+	# create random string to use it for password creation
+	random_list = [random.choice(random_chars + secret) for i in range(password_length)]
+	random_str = ' '.join(random_list)
+
+	# create password from sha1 of the radnom string
+	password = sha1(random_str.encode()).hexdigest()
+	return password
 
 if __name__ == "__main__":
 	# config logging  
@@ -22,18 +47,20 @@ if __name__ == "__main__":
 		sys.exit(-666)
 	
 	try:
-	  opts, args = getopt.getopt(args,"ha:s:",["account=","secret="])
+	  opts, args = getopt.getopt(args,"ha:s:",["help","account=","secret="])
 	except:
 		logging.warning("Usage : create.py -a <account_name> -s <secret_word>")
-		
 		sys.exit(-666)
+	
 	for opt, arg in opts:
-		if opt == '-h':
+		if opt in ("-h","--help"):
 			logging.info("Usage : create.py -a <account_name> -s <secret_word>")
 			sys.exit(-666)
 		elif opt in ("-a","--account"):
 			account = arg
 		elif opt in ("-s","--secret"):
 			secret = arg
-	logging.info(f"account = {account} , secret = {secret}")
+	# creating password 
+	password = create_pass(account,secret)
+	logging.info(f"account = {account} , password = {password}")
 	
